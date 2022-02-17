2d5d708046d991e6d787defaf4ccd443054a600e
AKENZJUNIOR <99854659+AKENZJUNIOR@users.noreply.github.com>
2022-02-17 10:16:03 +0700

    Add files via upload

---
+++ AKENZ.sh
@@ -0,0 +1,179 @@
+#!/system/bin/sh
+echo " 
+--╔════════════✧════════════╗
+--   📳 @AKENZJUNIOR
+
+--   📳 @KIMJONGCHANNEL
+--╚════════════✧════════════╝"
+echo " "
+echo " 📲 Add Aim Assist "
+echo " "
+sleep 2
+echo " 📲 Add Cpu Performance "
+echo " "
+sleep 2
+echo " 📲 Add 720p && 60fps "
+echo " "
+sleep 2
+echo " 📲 Add iPhone 13 Pro Max"
+echo " "
+sleep 5
+clear
+echo " ↘️ PROSESING INSTALL CONFIG..."
+sleep 5
+echo " "
+clear
+
+echo " 🔺 Add Unrandom Storage 🔺 "
+echo "///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////"
+echo "///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////"
+sleep 3
+
+echo " "
+echo "[94mCheck Device Model ⌛..."
+echo " "
+echo "[94m📲 Product Model : $(getprop ro.product.model)"
+
+echo "[94m📲 Product Brand : $(getprop ro.product.board)"
+
+echo "[94m📲 Product Time : $(getprop ro.vendor.build.date)"
+
+echo " "
+echo "[91m╭═════════════╮"
+
+echo "[91m║   [90m• ════    [91m║"
+
+echo "[91m║[90m█████████████[91m║   [94m╭═════════════════════════[91m✪"
+
+echo "[91m║[90m█████████████[91m║   [94m║"
+
+echo "[91m║[90m█████████████[91m╠[94m───╣  [91m⏪[93m◀[92m AKENZ PROJECT SH [93m▶[91m⏩"
+
+echo "[91m║[90m█████████████[91m║   [94m║"
+
+echo "[91m║[90m█████████████[91m║   [94m╰═════════════════════════[91m✪"
+
+echo "[91m║[90m█████████████[91m║"
+
+echo "[91m║[90m█████████████[91m║"
+
+echo "[91m║[90m█████████████[91m║"
+
+echo "[91m║[90m█████████████[91m║"
+sleep 1
+echo "[91m║[90m█████████████[91m║"
+
+echo "[91m║[90m█████████████[91m║"
+
+echo "[91m║ [92m<   [96m ✖    [92m> [91m║"
+
+echo "[91m╰═════════════╯"
+
+
+AKENZ() {
+echo "[UserCustom DeviceProfile]
++CVars=0B572C0A1C0B280C1815100D002A1C0D0D10171E4449
++CVars=0B5734161B10151C3A16170D1C170D2A1A18151C3F181A0D160B44485749
++CVars=0B5734180D1C0B101815280C1815100D00351C0F1C154449
++CVars=0B572A11181D160E280C1815100D004449
++CVars=0B572A11181D160E573418013A2A342B1C0A16150C0D101617444D
++CVars=0B572A11181D160E573A2A345734180134161B10151C3A180A1A181D1C0A4449
++CVars=0B572A11181D160E573D100A0D18171A1C2A1A18151C4449
++CVars=0B5734161B10151C573D00171814101A361B131C1A0D2A11181D160E4449
++CVars=0B573D1C090D11361F3F101C151D280C1815100D004449
++CVars=0B572B1C1F0B181A0D101617280C1815100D004449
++CVars=0B572A0D180D101A341C0A1135363D3D100A0D18171A1C2A1A18151C4448574A
++CVars=1F161510181E1C5735363D3D100A0D18171A1C2A1A18151C4449574F
++CVars=1F161510181E1C5734101735363D4449
++CVars=0B573D1C0D18101534161D1C4449
++CVars=0B573418013817100A160D0B1609004448
++CVars=0B572A0D0B1C181410171E57291616152A10031C444B4949
++CVars=0B573C14100D0D1C0B2A09180E172B180D1C2A1A18151C4449574C
++CVars=0B5729180B0D101A151C35363D3B10180A444B
++CVars=1D1000572A1C0D3D1C1A18153B181210171E2B2D2A10031C301735161B1B00444C484B
++CVars=0B570A16573418013D100A0D18171A1C3F160B3518171D0A1A18091C361A1A150C1D1C0B444849494949
++CVars=1E572E10171D160E573A0B1C180D1C341801444A
++CVars=0B5734161B10151C5735160E3D1C0F101A1C35363D3B10180A4448
++CVars=1F01573710181E180B18381515160E2B1C171D1C0B10171E4449
++CVars=0B57292C3B3E2F1C0B0A101617444C
++CVars=0B572C0A1C0B313D2B2A1C0D0D10171E4448
++CVars=0B5734161B10151C313D2B4449
++CVars=0B5734161B10151C572A1A1C171C3A1615160B3F160B14180D4449
++CVars=0B573B15161614280C1815100D004449
++CVars=0B5735101E110D2A11181F0D280C1815100D004449
++CVars=0B5734161B10151C572D16171C141809091C0B3F1015144449
++CVars=0B5734161B10151C5738150E18000A2B1C0A16150F1C3D1C090D114449
++CVars=0B572C0A1C0B342A38382A1C0D0D10171E4449
++CVars=0B5734161B10151C342A38384448
++CVars=0B573D1C1F180C150D3F1C180D0C0B1C5738170D10381510180A10171E4449
++CVars=0B5734161B10151C2A101409151C2A11181D1C0B4449
++CVars=0B5734161B10151C370C143D00171814101A291610170D35101E110D0A4449
++CVars=0B572C0A1C0B2F0C151218172A1C0D0D10171E4449
+
+
+" > /data/media/0/Android/data/$PKG/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/UserCustom.ini
+}
+echo
+echo "Automatically Config Installation"
+echo " "
+echo " ⚠️ Remove config manual ⚠️"
+
+if [ -d /data/media/0/Android/data/com.tencent.ig ] ;then
+PKG="com.tencent.ig"
+echo "Detected: $PKG"
+echo " "
+AKENZ ; sleep 3
+echo "Config Installed On:  $PKG"
+fi
+
+if [ -d /data/media/0/Android/data/com.pubg.krmobile ] ;then
+PKG="com.pubg.krmobile"
+echo "Detected: $PKG"
+echo " "
+AKENZ ; sleep 3
+echo "Config Installed On:  $PKG"
+fi
+
+if [ -d /data/media/0/Android/data/com.vng.pubgmobile ] ;then
+PKG="com.vng.pubgmobile"
+echo "Detected: $PKG"
+echo " "
+AKENZ ; sleep 3
+echo "Config Installed On:  $PKG"
+fi
+
+if [ -d /data/media/0/Android/data/com.rekoo.pubgm ] ;then
+PKG="com.rekoo.pubgm"
+echo "Detected: $PKG"
+echo " "
+AKENZ ; sleep 3
+echo "Config Installed On:  $PKG"
+fi
+
+if [ -d /data/media/0/Android/data/com.pubg.imobile ] ;then
+PKG="com.pubg.imobile"
+echo "Detected: $PKG"
+echo " "
+AKENZ ; sleep 3
+echo "Config Installed On:  $PKG"
+fi
+echo "[94m📲 Product Model : $(getprop ro.product.model)"
+
+echo "[94m📲 Product Brand : $(getprop ro.product.board)"
+
+echo "[94m📲 Product Time : $(getprop ro.vendor.build.date)"
+
+echo " 📂 Remove Folder Virus "
+echo " "
+
+echo " 📂 Remove File Tmp "
+echo " "
+
+echo " 📂 Remove Data base "
+echo " "
+sleep 5
+echo "///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////"
+echo "///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////"
+
+
+
